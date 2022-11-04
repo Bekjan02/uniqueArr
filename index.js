d12e10b4b80fe6e271846345be2d194f13c7e188
Bekjan02 <bayganaliyev@mail.ru>
2022-11-04 11:39:24 +0600

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+const uniqueArray = (arr) => {
+	const result = arr.reduce((prev, curr) => {
+		if (prev.every(item => item.Value.toUpperCase() !== curr.Value.toUpperCase())) {
+			return [...prev, curr]
+		}
+		return prev
+	}, [])
+	return result
+}
+
+module.exports = uniqueArray
